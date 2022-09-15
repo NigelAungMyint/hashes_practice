4e75c0f87744d2a3cfc3e2217804cf9d77686ce6
Nigel Aung-Myint <nigel.aungmyint@gmail.com>
2022-09-15 10:08:16 -0500

    Initial commit

---
+++ tweet.rb
@@ -0,0 +1,11 @@
+tweet1 = {
+  "username" => "Leira Sánchez",
+  "handle" => "MechEngSanchez",
+  "date" => "Jan 25, 2022",
+  "time" => "6:06 AM",
+  "comment" => "My M1 Macbook Pro arrived 🤓",
+  "likes" => "22"
+}
+puts tweet1["username"]
+puts tweet1["handle"]
+puts tweet1["comment"]
